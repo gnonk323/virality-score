0b9cd8bf805d946f0d0ef0577c9ef71358818378
tahutzley <thutzley1@gmail.com>
2024-12-02 19:00:45 -0500

    Spotify logic

--- venv/api.py
+++ venv/api.py
@@ -1,9 +1,15 @@
-from flask import Flask, jsonify, request
+from flask import Flask, request
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
+# Route to call spotify API to get requested song
+@app.route("/api/find-songs", methods=['POST'])
+def return_songs():
+    return find_songs(request.get_json(["song_request"]))
+
+# Route to run the model on the given parameters
 @app.route("/api/calculate-virality", methods=['POST'])
 def return_virality():
-    return calculate_virality(request.get_json(["spotifyInfo"]))
+    return calculate_virality(request.get_json(["song_info"]))
--- venv/spotify.py
+++ venv/spotify.py
@@ -10,3 +10,22 @@ redirect_uri = 'http://google.com/callback/'
 scope = "user-read-playback-state user-modify-playback-state playlist-modify-private playlist-modify-public"
 
 device_id = creds.device_id
+
+def find_songs(song, artist, num_songs):
+    query = f"track:{song} artist:{artist}"
+    results = spotifyObject.search(query, num_songs, 0, "track")
+    songs_dict = results['tracks']
+    song_items = songs_dict['items']
+    if song_items:
+       return song_items
+    else:
+        return None
+
+# Code after if statement to get the song_name and artist of the first song
+'''
+song_name = song_items[0]['name']
+artist = song_items[0]['artists'][0]['name']
+return {
+    'song_name': song_name,
+    'artist': artist,
+}
